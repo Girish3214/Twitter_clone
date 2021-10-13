cab71cd09413ea6e53c5362efb75b13b8f8c3567
Girish3214 <71925088+Girish3214@users.noreply.github.com>
2021-10-13 17:51:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import  CssBaseline from '@mui/material/CssBaseline';
+import Container  from '@mui/material/Container';
+import { createTheme, ThemeProvider, useMediaQuery } from '@mui/material';
+import { useMemo } from 'react';
+
 
 function App() {
+   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
+      <Container maxWidth="lg" disableGutters={true}>
+        Girish
+      </Container>
+    </ThemeProvider>
   );
 }
 
